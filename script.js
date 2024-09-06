49ffe0e7a4e8dbda94cdfb88199db1c4267b17c7
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-06 19:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function getDomLevel(elementId) {
+
+    const element = document.getElementById(elementId);
+    
+  
+    if (!element) {
+      console.log("Element not found");
+      return -1;
+    }
+  
+   
+    let level = 0;
+    let currentElement = element;
+  
+  
+    while (currentElement) {
+      level++;
+      currentElement = currentElement.parentElement;
+    }
+  
+    return level; 
+  }
+  
+ 
+  const domLevel = getDomLevel('level');
+  if (domLevel !== -1) {
+  alert(`dom level is ${domLevel}`)
+  }
+  
